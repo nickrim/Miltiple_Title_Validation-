b5194ec1ccc32b0bc08ab60ea3ea21845ee9c3b2
nickrim <anisim@gmail.com>
2014-05-01 21:49:28 -0700

    HW 34 commit

---
+++ src/main/java/Miltiple_Title_Validation/App.java
@@ -0,0 +1,13 @@
+package Miltiple_Title_Validation;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
